cdbc7abf8397a52cd3daa7f67387d94edf2274c2
vikingcjh <vikingcjh@sina.com>
2017-08-09 17:22:26 +0800

    添加ffmpeg so库

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,19 @@
 #include <jni.h>
 #include <string>
 
+extern "C"{
+#include "libavcodec/avcodec.h"
+}
+
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_soul_learn_ffmpegsharelib_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+    char info[10000] = { 0 };
+    sprintf(info, "%s\n", avcodec_configuration());
+    return env->NewStringUTF(info);
+
+    /*std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());*/
 }
